0f871e49be5c19420df063536ce30648b39f98c2
Davi Alves <252664395+DaviAlvesDev@users.noreply.github.com>
2026-02-02 17:12:21 -0300

    :sparkles: feat: create user controller

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,70 @@
+import { InvalidNewUserDataError, InvalidUpdateUserDataError, InvalidUserIDError, UserNotFoundError } from '../errors/user-erros.js'
+import * as userServices from '../services/user-services.js'
+import type { Request, Response } from 'express'
+
+export async function getUsers(req: Request, res: Response) {
+    const users = await userServices.searchUsers()
+
+    res.json({
+        ok: true,
+        data: users
+    })
+}
+
+export async function getUserByID(req: Request, res: Response) {
+    const { id } = req.params
+    const userID = Number(id) || undefined
+    if (!userID) throw new InvalidUserIDError()
+
+    const user = await userServices.searchUsers(userID)
+
+    res.json({
+        ok: true,
+        data: user
+    })
+}
+
+export async function postUser(req: Request, res: Response) {
+    const { email, password } = req.body
+
+    if (!email || !password) throw new InvalidNewUserDataError()
+
+    const createdUser = await userServices.registerNewUser(email, password)
+    res.status(201)
+        .json({
+            ok: true,
+            data: createdUser
+        })
+}
+
+export async function patchUser(req: Request, res: Response) {
+    const { id } = req.params
+    const userID = Number(id) || undefined
+    if (!userID) throw new InvalidUserIDError()
+
+    const { email, password } = req.body
+    if (!email && !password) throw new InvalidUpdateUserDataError()
+
+    const newData = {
+        email,
+        password
+    }
+
+    const updatedUser = await userServices.updateUserData(userID, newData)
+    res.json({
+        ok: true,
+        data: updatedUser
+    })
+}
+
+export async function deleteUser(req: Request, res: Response) {
+    const { id } = req.params
+    const userID = Number(id) || undefined
+    if (!userID) throw new InvalidUserIDError()
+
+    const deletedUser = await userServices.deleteUserData(userID)
+    res.json({
+        ok: true,
+        data: deletedUser
+    })
+}
--- src/errors/user-erros.ts
+++ src/errors/user-erros.ts
@@ -29,3 +29,21 @@ export class UserNotFoundError extends AppError {
         super('The searched user does not exists or was already deleted', 404)
     }
 }
+
+export class InvalidUserIDError extends AppError {
+    constructor() {
+        super('Please search for a valid ID', 400)
+    }
+}
+
+export class InvalidNewUserDataError extends AppError {
+    constructor() {
+        super('Please send both "email" and "password" to post a new user', 400)
+    }
+}
+
+export class InvalidUpdateUserDataError extends AppError {
+    constructor() {
+        super('Please send either "email" or "password" to update the user data', 400)
+    }
+}
