f18baf1ab0cbccfeecd4e49cb559793ef942687a
Davi Alves <252664395+DaviAlvesDev@users.noreply.github.com>
2026-02-03 18:04:48 -0300

    :sparkles: feat: add RBAC to users routes

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -1,15 +1,21 @@
-import type { Request, Response, NextFunction } from "express"
+import type { Request, Response, NextFunction, RequestHandler } from "express"
 import { UnauthorizedUserError, UserNotLoggedError } from "../errors/user-erros.js"
-import jwt from 'jsonwebtoken'
-import 'dotenv/config'
 
-export async function authorize(allowedRoles = ['admin']) {
+export function authorize(allowedRoles = ['admin']): RequestHandler {
     return (req: Request, res: Response, next: NextFunction) => {
         const role = req.headers['x-user-role'] as string || ''
         if (!role) throw new UserNotLoggedError()
 
         if (!allowedRoles.includes(role)) throw new UnauthorizedUserError()
 
-        next()
+        if (role === 'admin') return next()
+
+        if (!allowedRoles.includes('owner')) return next()
+
+        const userID = req.headers['x-user-id'] as string || ''
+        const { id } = req.params
+        if (Number(id) !== Number(userID)) throw new UnauthorizedUserError()
+
+        return next()
     }
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,16 +1,17 @@
 import { Router } from "express"
 import { deleteUser, getUserByID, getUsers, patchUser, postUser } from "../controllers/user-controller.js"
+import { authorize } from "../middlewares/authorization.js"
 
 const userRouter = Router()
 
-userRouter.get('/all', getUsers)
+userRouter.get('/all', authorize(['admin']), getUsers)
 
-userRouter.get('/profile/:id', getUserByID)
+userRouter.get('/profile/:id', authorize(['owner', 'admin']), getUserByID)
 
 userRouter.post('/register', postUser)
 
-userRouter.patch('/update/:id', patchUser)
+userRouter.patch('/update/:id', authorize(['owner']), patchUser)
 
-userRouter.delete('/delete/:id', deleteUser)
+userRouter.delete('/delete/:id', authorize(['owner', 'admin']), deleteUser)
 
 export default userRouter
